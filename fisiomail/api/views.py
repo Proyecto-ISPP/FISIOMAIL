8d60eae56a7dc99cf6fe7538e43e2ff4f47f7eeb
danvelcam <danvelcam@alum.us.es>
2025-04-04 19:25:29 +0200

    feat: añadidas nuevas cabeceras para llegar a bandeja de entrada

--- fisiomail/api/views.py
+++ fisiomail/api/views.py
@@ -11,7 +11,8 @@ from django.core.mail import send_mail
 import base64
 import os
 from rest_framework.decorators import api_view
-import random 
+import random
+
 
 class SendEmailAPI(APIView):
     def post(self, request, *args, **kwargs):
@@ -19,7 +20,7 @@ class SendEmailAPI(APIView):
         api_key = request.headers.get("X-API-Key")
         if api_key != settings.API_KEY:
             return Response({"detail": "Unauthorized"}, status=status.HTTP_401_UNAUTHORIZED)
-        
+
         # Deserializar los datos cifrados
         serializer = EmailDataSerializer(data=request.data)
         if serializer.is_valid():
@@ -27,27 +28,35 @@ class SendEmailAPI(APIView):
             encrypted_subject = serializer.validated_data['encrypted_subject']
             encrypted_recipient = serializer.validated_data['encrypted_recipient']
             encrypted_body = serializer.validated_data['encrypted_body']
-            
+
             # Desencriptar los datos
             subject = self.decrypt_data(encrypted_subject)
             recipient = self.decrypt_data(encrypted_recipient)
             body = self.decrypt_data(encrypted_body)
             # Aquí puedes proceder a enviar el correo con Django (django.core.mail)
             # Enviar correo (usando Django Mail)
-            
+
             email = EmailMessage(
-             subject=subject,
-             body=body,
-             from_email=settings.EMAIL_HOST_USER,
-             to=[recipient],
+                subject=subject,
+                body=body,
+                from_email=settings.EMAIL_HOST_USER,
+                to=[recipient],
             )
             email.content_subtype = "html"
+            email.extra_headers = {
+                'Content-Transfer-Encoding': '8bit',
+                'Content-Type': 'text/html; charset=UTF-8',
+                'Reply-To': 'citas@fisiofind.com',
+                'X-Mailer': 'FisioMail v1.0',
+                'X-Content-Type-Options': 'nosniff',
+                'X-Frame-Options': 'DENY',
+                'X-XSS-Protection': '1; mode=block',
+                'List-Unsubscribe': 'mailto:unsubscribe@fisiofind.com',
+            }
             email.send()
             return Response(status=status.HTTP_200_OK)
-            
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
     def decrypt_data(self, encrypted_data):
         # Desencriptar los datos utilizando la clave del entorno (AES)
@@ -58,14 +67,17 @@ class SendEmailAPI(APIView):
 
         # Extraer el IV (primeros 16 bytes de los datos cifrados)
         iv = encrypted_data_bytes[:16]  # El IV está al principio
-        encrypted_data_bytes = encrypted_data_bytes[16:]  # Los datos cifrados están después del IV
+        # Los datos cifrados están después del IV
+        encrypted_data_bytes = encrypted_data_bytes[16:]
 
         # Desencriptar usando AES (Cifrado simétrico)
-        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+        cipher = Cipher(algorithms.AES(key), modes.CBC(iv),
+                        backend=default_backend())
         decryptor = cipher.decryptor()
 
         # Desencriptar los datos
-        decrypted_data = decryptor.update(encrypted_data_bytes) + decryptor.finalize()
+        decrypted_data = decryptor.update(
+            encrypted_data_bytes) + decryptor.finalize()
 
         # Eliminar el padding con PKCS7
         unpadder = padding.PKCS7(128).unpadder()  # 128 bits = 16 bytes
